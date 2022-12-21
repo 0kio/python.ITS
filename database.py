263dec449bb0ce5526a834e1100509f0fcfc6aee
Your Name <you@example.com>
2022-12-21 12:46:12 +0600

    Modified templates to display posts from database.

---
+++ database.py
@@ -0,0 +1,15 @@
+# users = {}
+# login = input('введите логин ')
+# password = input('введите пароль ')
+# password2 = input('подтвердите пароль  ')
+# if login not in users:
+#     if  password2 == password:
+#         print('вы приняты')
+#         if password == password2:
+#          print('такого пароля не существует.')
+#          print('можете регестрацию пройти.')
+#          if login not in users:
+#           login = input('введите логин ')
+# password = input('введите пароль ')
+# password2 = input('подтвердите пароль  ')
+# print('успех')
